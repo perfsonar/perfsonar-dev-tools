f623e5fbaa3b7a444f60f8d4465402b97875b173
Antoine Delvaux <antoine.delvaux@gmail.com>
2020-12-16 10:50:17 +0000

    When reseting debian/changelog do it differently for pscheduler packages.

--- bin/reset-debian.sh
+++ bin/reset-debian.sh
@@ -4,16 +4,21 @@ WHEREAMI=$(dirname $0)
 . "${WHEREAMI}/common"
 
 # Version to reverse to (TAG)
-VERSION="stretch/4.3.0"
+VERSION="stretch/4.3.2"
 
 # Get current branch
 BRANCH=`git branch --list | awk '/^\* .*$/ {print $2}'`
 
 # Reverse faulty changelog entries
-if grep -qs '(native)' debian/source/format ; then
-    git checkout debian/${VERSION} -- debian/changelog
+if [ -f "debian/source/format" ]; then
+    if grep -qs '(native)' debian/source/format ; then
+        git checkout debian/${VERSION} -- debian/changelog
+    else
+        git checkout debian/${VERSION}-1 -- debian/changelog
+    fi
 else
-    git checkout debian/${VERSION}-1 -- debian/changelog
+    # We're in multi package repository (pscheduler), shouldn't contain any source package
+    find . -type f -name changelog -exec git checkout debian/${VERSION}-1 -- {} \;
 fi
 
 # And generate debian/changelog and debian/gbp.conf again
